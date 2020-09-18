62751a454563c710505f040cc8a9dd639cab1d6d
Rido Ca <35163844+RidoCa@users.noreply.github.com>
2020-09-18 18:04:00 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,87 +1,32 @@
 <?php
 
-namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-use Auth;
-use App\Models\User;
-use Illuminate\Support\Str;
-use Validator;
-use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Route;
 
-class authController extends Controller
-{
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-    public $successStatus = 200;
+Route::post('login', 'authController@login');
+Route::post('register', 'authController@register');
+Route::resource('aset', 'asetController');
+Route::get('all', 'asetController@index');
+Route::get('all/{id}', 'asetController@show');
+Route::post('new', 'asetController@store');
+Route::post('update', 'asetController@update');
+Route::post('del/{id}', 'asetController@destroy');
 
-    public function login(Request $request){
-        $data = [
-            'username' => $request->username,
-            'password' => $request->password
-        ];
- 
-        if (Auth::attempt($data)) {
-            $user = Auth::user();
-            $success['key_login'] =  $user->createToken('LaravelAuthApp')->accessToken;
-            $success['role'] =  auth()->user()->role;
-            return response()->json(['token' => $success], 200);
-        } else {
-            return response()->json(['error' => 'Unauthorised'], 401);
-        }
-    }
-    
-    public function lupa(Request $request){
-            $dataUser = User::where([['username','=',$request->username],['email','=',$request->email]])->first();
-            
-        if($dataUser!=null){
-            $success['key_login'] =  $dataUser->createToken('LaravelAuthApp')->accessToken;
-            $success['new_password'] =  Str::random(8);
-            return response()->json(['success' => $success], $this->successStatus);
-        }
-        else{
-            return response()->json(['error'=>'Data tidak ada'], 401);
-        }
-    }
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('logout', 'authController@logout');
+}); 
 
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|alpha',
-            'username' => 'required|unique:App\Models\User,username',
-            'email' => 'required|email|unique:App\Models\User,email',
-            'phone' => 'required|numeric|unique:App\Models\User,phone',
-            'address' => 'required',
-            'role' => 'required|alpha',
-            'password' => 'required',
-            'c_password' => 'required|same:password',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);            
-        }
-
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $token = $user->createToken('LaravelAuthApp')->accessToken;
-        $success['key_login'] =  $dataUser->createToken('LaravelAuthApp')->accessToken;
-        $success['new_password'] =  'berhasil login';
-        return response()->json(['success'=>$success], $this->successStatus);
-    }
-
-    public function logout()
-    {
-        $logout = Auth::user()->token()->revoke();
-        if($logout){
-            return response()->json([
-                'message' => 'Successfully logged out'
-            ]);
-        }
-    }
-    
-    public function masuk()
-    {
-        return view('welcome');
-    }
- 
-}
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
